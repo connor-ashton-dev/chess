4a6d79a9a31c647fa7de1545e47b5491b5faf945
Connor Ashton <cjconnorashton@gmail.com>
2024-02-27 10:59:11 -0700

    finished unit tests

--- server/src/test/java/serviceTests/AuthServiceTest.java
+++ server/src/test/java/serviceTests/AuthServiceTest.java
@@ -1,4 +1,69 @@
 package serviceTests;
 
-public class AuthServiceTest {
+import dataAccess.DataAccessException;
+import dataAccess.MemObj;
+import dataAccess.UserDAO;
+import model.AuthData;
+import model.UserData;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import service.AuthService;
+import service.UserService;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AuthServiceTest {
+    UserDAO dao;
+    @BeforeEach
+    void prep(){
+        dao = new UserDAO();
+        MemObj.MemClear();
+    }
+
+    @Test
+    void loginGood() {
+        var testUser = new UserData("u", "p", "e");
+        var authService = new AuthService(dao);
+        var userService = new UserService(dao);
+
+        assertDoesNotThrow(() -> userService.register(testUser));
+
+        var tok = assertDoesNotThrow(() -> authService.login(testUser));
+
+        assertNotNull(tok);
+        assertNotNull(tok.getAuthToken());
+        assertNotNull(tok.getUsername());
+    }
+
+    @Test
+    void loginUserNotFound(){
+        var testUser = new UserData("u", null, "e");
+        var authService = new AuthService(dao);
+        var err = assertThrows(DataAccessException.class, () -> authService.login(testUser));
+        assertTrue(err.getMessage().toLowerCase().contains("unauthorized"));
+    }
+
+    @Test
+    void logoutGood() {
+        var testUser = new UserData("u", "p", "e");
+        var authService = new AuthService(dao);
+        var userService = new UserService(dao);
+
+        assertDoesNotThrow(() -> userService.register(testUser));
+
+        var tok = assertDoesNotThrow(() -> authService.login(testUser));
+
+        assertDoesNotThrow(() -> authService.logout(tok));
+    }
+
+
+    @Test
+    void logoutBad(){
+        var authService = new AuthService(dao);
+        var authToken = new AuthData("username");
+
+        var err = assertThrows(DataAccessException.class, () -> authService.logout(authToken));
+        assertTrue(err.getMessage().toLowerCase().contains("unauthorized"));
+    }
+
 }
--- server/src/test/java/serviceTests/GameServiceTest.java
+++ server/src/test/java/serviceTests/GameServiceTest.java
@@ -1,4 +1,91 @@
 package serviceTests;
 
-public class GameServiceTest {
+import dataAccess.DataAccessException;
+import dataAccess.GameDAO;
+import dataAccess.MemObj;
+import dataAccess.UserDAO;
+import model.AuthData;
+import model.GameData;
+import model.UserData;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import service.GameService;
+
+class GameServiceTest {
+    GameDAO gDao;
+    UserDAO uDao;
+    UserData user;
+    GameService gameService;
+
+    GameData newGame = new GameData(240, "", "", "", null);
+
+    @BeforeEach
+    void prep() {
+        user = new UserData("u", "p", "e");
+
+        gDao = new GameDAO();
+        uDao = new UserDAO();
+        gameService = new GameService();
+
+        MemObj.MemClear();
+    }
+
+
+    @Test
+    void listGamesGood() {
+        var tok = assertDoesNotThrow(() -> uDao.insertUser(user));
+        var game = assertDoesNotThrow(() -> gDao.createGame(tok, newGame));
+        var allGames = assertDoesNotThrow(() -> gameService.listGames(tok));
+        assertTrue(allGames.contains(game));
+    }
+
+
+    @Test
+    void listGamesBad() {
+        var tok = assertDoesNotThrow(() -> uDao.insertUser(user));
+        assertDoesNotThrow(() -> gDao.createGame(tok, newGame));
+
+        var badToken = new AuthData(user.getUsername());
+
+        var err = assertThrows(DataAccessException.class, () -> gameService.listGames(badToken));
+        assertTrue(err.getMessage().toLowerCase().contains("unauthorized"));
+    }
+
+    @Test
+    void joinGameGood() {
+        var tok = assertDoesNotThrow(() -> uDao.insertUser(user));
+        var gameToInsert = new GameData(234234, "wUsername", null, "sickGame", null);
+
+        var game = assertDoesNotThrow(() -> gDao.createGame(tok, gameToInsert));
+
+        assertDoesNotThrow(() -> gameService.joinGame(tok, new GameData(game.getGameId(), null, user.getUsername(), game.getGameName(), null)));
+    }
+
+    @Test
+    void joinGameBad() {
+        var tok = assertDoesNotThrow(() -> uDao.insertUser(user));
+        var gameToInsert = new GameData(234234, "wUsername", null, "sickGame", null);
+
+        var game = assertDoesNotThrow(() -> gDao.createGame(tok, gameToInsert));
+
+        var err = assertThrows(DataAccessException.class, () -> gameService.joinGame(new AuthData(user.getUsername()), new GameData(game.getGameId(), null, user.getUsername(), game.getGameName(), null)));
+        assertTrue(err.getMessage().toLowerCase().contains("unauthorized"));
+    }
+
+    @Test
+    void createGameGood() {
+        var tok = assertDoesNotThrow(() -> uDao.insertUser(user));
+        assertDoesNotThrow(() -> gameService.createGame(tok, newGame));
+    }
+
+    @Test
+    void createGameBad() {
+        var newUser = new AuthData(user.getUsername());
+        var err = assertThrows(DataAccessException.class, () -> gameService.createGame(newUser, newGame));
+        assertTrue(err.getMessage().toLowerCase().contains("unauthorized"));
+    }
+
 }

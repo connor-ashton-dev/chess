a57db9fc2dfb65eda08231316bfc20ef366b0ad5
Connor Ashton <cjconnorashton@gmail.com>
2024-03-21 11:13:17 -0600

    server shenanigans

--- client/src/test/java/clientTests/ServerFacadeTests.java
+++ client/src/test/java/clientTests/ServerFacadeTests.java
@@ -2,8 +2,11 @@ package clientTests;
 
 import model.AuthData;
 import model.UserData;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import server.Server;
 import serverFacade.ServerFacade;
 import ui.ClientException;
 
@@ -13,6 +16,18 @@ class ServerFacadeTests {
 
     ServerFacade serverFacade = new ServerFacade();
     UserData testUser = new UserData("uniqueUser", "password123", "email@test.com");
+    private static Server server;
+
+    @BeforeAll
+    public static void init() {
+        server = new Server();
+        var port = server.run(8080);
+        System.out.println("Started test HTTP server on " + port);
+    }
+    @AfterAll
+    static void stopServer() {
+        server.stop();
+    }
 
     @BeforeEach
     void setupEnvironment() {

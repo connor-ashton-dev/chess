f2cc9575da744dfa05941c936bbe7a0d686c55f9
Connor Ashton <cjconnorashton@gmail.com>
2024-04-11 10:02:29 -0600

    notification message

---
+++ shared/src/main/java/webSocketMessages/serverMessages/NotificationMessage.java
@@ -0,0 +1,19 @@
+package webSocketMessages.serverMessages;
+
+public class NotificationMessage extends ServerMessage {
+    private final String message;
+
+    public NotificationMessage(String message) {
+        super(ServerMessageType.NOTIFICATION);
+        this.message=message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    @Override
+    public String toString() {
+        return STR."NotificationMessage{message='\{message}\{'\''}\{'}'}";
+    }
+}
